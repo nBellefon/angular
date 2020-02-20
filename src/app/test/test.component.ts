d72b1ab364fa42a625e6235df3ae6abde1593644
nBellefon <noebellefon@gmail.com>
2020-02-20 10:17:48 +0100

    3 files modified

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { AuthService, Film } from '../@shared/auth.service';
+import { ConnexionService, User } from '../@shared/connexion.service';
 
 @Component({
   selector: 'app-test',
@@ -11,11 +12,13 @@ import { AuthService, Film } from '../@shared/auth.service';
 export class TestComponent implements OnInit {
   filmForm : FormGroup;
   films : Film [];
+  pseudo : string ;
 
   constructor(
     private fb: FormBuilder,
-    private authService: AuthService
-    ) { 
+    private filmService: AuthService,
+    private authService : ConnexionService
+    ) {
 
     this.filmForm = this.fb.group({  // Crée une instance de FormGroup
       nom: '',                   // Crée une instance de FormControl
@@ -25,14 +28,14 @@ export class TestComponent implements OnInit {
   }
 
   ngOnInit(){
-    this.films = this.authService.getFilms();
+    this.films = this.filmService.getFilms();
+    this.pseudo = this.authService.getPseudo();
   }
 
   newFilm() {
     
     console.log('Données du formulaire...', this.filmForm.value);
-    this.authService.auth(this.filmForm.value.nom, this.filmForm.value.genre,this.filmForm.value.real, "0", 0);
-    
+    this.filmService.auth(this.filmForm.value.nom, this.filmForm.value.genre,this.filmForm.value.real, "0", 0);
   }
 
   like(id : number){
